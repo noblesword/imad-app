6e1bd25a4cfc739fe75d2ec10facd9210c181d82
noblesword <30838498+noblesword@users.noreply.github.com>
2018-02-22 22:03:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ app.get('/article1', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article1.html'));
 });
 app.get('/article2', function (req, res) {
-  res.send('A2');
+  res.sendFile(path.join(__dirname, 'ui', 'article2.html'));
 });
 app.get('/article3', function (req, res) {
-  res.send('A3');
+  res.sendFile(path.join(__dirname, 'ui', 'article3.html'));
 });
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
